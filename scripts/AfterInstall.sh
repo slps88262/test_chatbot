971d70f2eb113639e90bad012e4ed5c65e2f338e
unknown <slps882626@gmail.com>
2019-05-13 02:09:05 +0800

    3rd

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,15 +1,9 @@
-#!/bin/bash -ex
-
-#將sercretFile從s3下載
+#!/bin/bash
 cd /home/ec2-user/deploy/code/
 aws s3 cp s3://dc101-project02/secretFile.txt .
-
-#將database從s3下載
 cd /home/ec2-user/deploy/
 aws s3 cp s3://dc101-project02/mysql_data.tar .
-sudo tar -xvf mysql_data.tar
-sudo rm mysql_data.tar
+tar -xvf mysql_data.tar
+rm mysql_data.tar
 
 exit 0
-~
-
